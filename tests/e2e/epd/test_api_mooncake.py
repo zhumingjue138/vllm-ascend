ac291a03c00932bf201bd23b4e84434cdf09b91e
z00939163 <zhumingjue@huawei.com>
2026-01-08 17:10:59 +0800

    修改mooncake

--- tests/e2e/epd/test_api_mooncake.py
+++ tests/e2e/epd/test_api_mooncake.py
@@ -9,7 +9,8 @@ from tests.e2e.conftest import RemoteOpenAIServer
 from tests.e2e.conftest import RemoteEPDServer
 from tests.e2e.epd.conftest import load_config
 from tools.aisbench import run_aisbench_cases
-from tests.e2e.nightly.multi_node.config.multi_node_epd_config import ClusterManager
+from tests.e2e.nightly.multi_node.config.utils import get_cluster_ips
+from tests.e2e.nightly.multi_node.config.multi_node_epd_config import ClusterManager, EnvManager
 
 model_path = load_config().get("model_path")
 MODELS = [os.path.join(model_path, "Qwen2.5-VL-7B-Instruct")]
